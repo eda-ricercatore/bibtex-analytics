0cbd8bb6640f5343c27d275bfa3b4d2629d5d303
Zhiyang Ong <ongz@acm.org>
2017-12-14 17:33:55 -0600

    Sorting (key,frequency) pairs.

---
+++ database/key_frequency_pairs.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+###	/Library/Frameworks/Python.framework/Versions/3.6/bin/python3
+
+
+"""
+	This Python script is written by Zhiyang Ong to sort a set of
+		(key,frequency) pairs, using their keys.
+	
+	
+	Synopsis:
+	Process input arguments for a script to clean BibTeX databases.
+
+
+	Notes/Assumptions:
+	Do not consider the 'Annote' field as a 'standard' BibTeX field.
+
+	Assume that each 'standard' BibTeX field is a single line field.
+
+	If the BibTeX field 'Annote' has multiple lines, only its last
+		line will end with '}}' and that 'Annote' is the last field
+		of the BibTeX entry.
+
+	Revision History:
+	April 14, 2017			Version 0.2, initial build.
+"""
+
+__author__ = 'Zhiyang Ong'
+__version__ = '1.0'
+__date__ = 'Apr 14, 2017'
+
+#	The MIT License (MIT)
+
+#	Copyright (c) <2014-2017> <Zhiyang Ong>
+
+#	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+#	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+#	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#	Email address: echo "cukj -wb- 23wU4X5M589 TROJANS cqkH wiuz2y 0f Mw Stanford" | awk '{ sub("23wU4X5M589","F.d_c_b. ") sub("Stanford","d0mA1n"); print $5, $2, $8; for (i=1; i<=1; i++) print "6\b"; print $9, $7, $6 }' | sed y/kqcbuHwM62z/gnotrzadqmC/ | tr 'q' ' ' | tr -d [:cntrl:] | tr -d 'ir' | tr y "\n"	Che cosa significa?
+
