cb90e438f0eee1d1ae7478de1090f3a28d9d791b
Zhiyang Ong <ongz@acm.org>
2017-12-15 17:36:16 -0600

    Sorting (key,frequency) pairs.

--- database/key_frequency_pairs.py
+++ database/key_frequency_pairs.py
@@ -8,25 +8,16 @@
 	
 	
 	Synopsis:
-	Process input arguments for a script to clean BibTeX databases.
+	Sort a set of (key,frequency) pairs, using their keys.
 
-
-	Notes/Assumptions:
-	Do not consider the 'Annote' field as a 'standard' BibTeX field.
-
-	Assume that each 'standard' BibTeX field is a single line field.
-
-	If the BibTeX field 'Annote' has multiple lines, only its last
-		line will end with '}}' and that 'Annote' is the last field
-		of the BibTeX entry.
 
 	Revision History:
-	April 14, 2017			Version 0.2, initial build.
+	December 15, 2017			Version 0.1, initial build.
 """
 
 __author__ = 'Zhiyang Ong'
 __version__ = '1.0'
-__date__ = 'Apr 14, 2017'
+__date__ = 'December 15, 2017'
 
 #	The MIT License (MIT)
 
@@ -39,4 +30,19 @@ __date__ = 'Apr 14, 2017'
 #	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #	Email address: echo "cukj -wb- 23wU4X5M589 TROJANS cqkH wiuz2y 0f Mw Stanford" | awk '{ sub("23wU4X5M589","F.d_c_b. ") sub("Stanford","d0mA1n"); print $5, $2, $8; for (i=1; i<=1; i++) print "6\b"; print $9, $7, $6 }' | sed y/kqcbuHwM62z/gnotrzadqmC/ | tr 'q' ' ' | tr -d [:cntrl:] | tr -d 'ir' | tr y "\n"	Che cosa significa?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
