7167a92291d5c3d97442413975017d6e2731375b
Zhiyang Ong <ongz@acm.org>
2018-01-05 15:59:59 -0600

    Update incremental & automated regression test suite,

--- database/key_check.py
+++ database/key_check.py
@@ -180,8 +180,9 @@ class check_bibtex_key:
 	@staticmethod
 	def tokenization_entry_key(str):
 		tokenized_BibTeX_entry = re.split('@|{|,',str)
-		a = str(len(tokenized_BibTeX_entry))
-		print "=	length of tokenized_BibTeX_entry:"+str(len(tokenized_BibTeX_entry)) 
+		#a = str(len(tokenized_BibTeX_entry))
+		#print "=	length of tokenized_BibTeX_entry:"+str(len(tokenized_BibTeX_entry))
+		print "=	length of tokenized_BibTeX_entry:%d\n",len(tokenized_BibTeX_entry) 
 		if len(tokenized_BibTeX_entry) > 3:
 			raise Exception("	Non-compliance to BibTeX guidelines!!!")
 		elif len(tokenized_BibTeX_entry) == 3:
